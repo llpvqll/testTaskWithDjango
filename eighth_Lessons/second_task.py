072930ad1770715e2983bd2d92c877d1dc852fc2
llpvoll <llpvo2602ll@gmail.com>
2021-07-28 18:57:13 +0300

    add first and second task

---
+++ eighth_Lessons/second_task.py
@@ -0,0 +1,32 @@
+
+
+
+
+class Books:
+    def __init__(self, author, book_name, publishing_year, book_genre):
+        self.author = author
+        self.book_name = book_name
+        self.publishing_year = publishing_year
+        self.book_genre = book_genre
+        self.comments_list = []
+
+    def __repr__(self):
+        return f'\nAuthor: {self.author} \nBook name: {self.book_name} \nThe year of publishing: {self.publishing_year} \nBook genre: {self.book_genre}\nComments: {self.comments_list}'
+
+    def __str__(self):
+        return f'\nAuthor: {self.author} \nBook name: {self.book_name} \nThe year of publishing: {self.publishing_year} \nBook genre: {self.book_genre}\nComments: {self.comments_list}'
+
+    def __eq__(self, other):
+        return isinstance(other, Books) and Books.__repr__(self) == Books.__str__(self)
+
+
+first_Book = Books('Bob', "Bob's history", 2000, 'History')
+second_Book = Books('Alfred', 'Interesting history about Lion', 1992, 'Drama')
+
+if first_Book == second_Book:
+    print(first_Book)
+    print(second_Book)
+else:
+    print(second_Book)
+
+
